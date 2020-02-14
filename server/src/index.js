5e1f7a1c4b74f2bd899c1789dbb290a62e447cd8
Leo So <leosohc@gmail.com>
2020-02-15 02:20:57 +0800

    basic graphql added

--- server/src/index.js
+++ server/src/index.js
@@ -3,6 +3,8 @@ const morgan = require('morgan');
 const helmet = require('helmet');
 const cors = require('cors');
 const middlewares = require('./middlewares');
+const graphqlHttp = require('express-graphql');
+const { buildSchema } = require('graphql');
 
 const app = express();
 const port = process.env.PORT || 1337;
@@ -18,6 +20,33 @@ app.get('/', (req, res) => {
     message: 'Hello World'
   })
 });
+
+app.use('/graphql', graphqlHttp({
+  schema: buildSchema(`
+    type RootQuery{
+      users: [String!]!
+    }
+
+    type RootMutation{
+      createUser(name: String): String
+    }
+
+    schema{
+      query: RootQuery
+      mutation: RootMutation
+    }
+  `),
+  rootValue: {
+    users: () => {
+      return ['Romantic', 'hehe', 'gg'];
+    },
+    createUser: (args) => {
+      const eventName = args.name;
+      return eventName;
+    }
+  },
+  graphiql: true
+}))
 
 app.use(middlewares.notFound);
 app.use(middlewares.errorHandler);
