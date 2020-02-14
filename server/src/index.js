497305a25b8fbd86d89a2377c76948eaea6881eb
Leo So <leosohc@gmail.com>
2020-02-14 22:30:54 +0800

    setup the error handler

--- server/src/index.js
+++ server/src/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const morgan = require('morgan');
 const helmet = require('helmet');
 const cors = require('cors');
+const middlewares = require('./middlewares');
 
 const app = express();
 const port = process.env.PORT || 1337;
@@ -11,6 +12,17 @@ app.use(helmet());
 app.use(cors({
   origin: 'http://localhost:3000',
 }));
+
+app.get('/', (req, res) => {
+  res.json({
+    message: 'Hello World'
+  })
+});
+
+app.use(middlewares.notFound);
+app.use(middlewares.errorHandler);
+
+
 
 app.listen(port, () => {
   console.log(`Listening at http://localhost:${port}`);
